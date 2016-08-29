2b6fc6bc5bbc76c35c33bf2dffb7db38754cbfb3
Morgan Engel <mrpoptart@gmail.com>
2016-08-29 13:44:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ controller.hears(['boob', 'tit', 'hooter'], ['ambient'], function (bot, message)
 controller.hears(['dick', 'cock', 'shaft', 'pud', 'shaft', 'wang', 'penis', 'dong', 'pud', 'weiner', 'rod'], ['ambient'], function (bot, message) {
 	bot.reply(message, '8' + Array(Math.floor(Math.random() * 10)).join("=") + '>')
 })
+})
+
+controller.hears(['huge dick', 'big dick', 'giant dick'], ['ambient'], function (bot, message) {
+	bot.reply(message, '8======================>')
+})
 
 controller.hears(['vag', 'cunt', 'snatch', 'pussy', 'twat', 'hairy hatchet wound'], ['ambient'], function (bot, message) {
 	bot.reply(message, '({|})')
