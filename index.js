51d7f9b55a484ba0662b3080c3e16dd6a0898196
Morgan Engel <mrpoptart@gmail.com>
2016-09-14 13:22:05 -0700

    Update index.js

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ controller.hears(['lesbians'], ['ambient'], function (bot, message) {
 	bot.reply(message, '```HEY! YOU JUST SAID THE MAGIC WORD\n\n888     8888888888 .d8888b. 888888b. 8888888       d8888888b    888 .d8888b.\n888     888       d88P  Y88b888  "88b  888        d888888888b   888d88P  Y88b\n888     888       Y88b.     888  .88P  888       d88P88888888b  888Y88b.\n888     8888888    "Y888b.  8888888K.  888      d88P 888888Y88b 888 "Y888b.\n888     888           "Y88b.888  "Y88b 888     d88P  888888 Y88b888    "Y88b.\n888     888             "888888    888 888    d88P   888888  Y88888      "888\n888     888       Y88b  d88P888   d88P 888   d8888888888888   Y8888Y88b  d88P\n888888888888888888 "Y8888P" 8888888P"8888888d88P     888888    Y888 "Y8888P"\n```')
 })
 
+controller.hears(['goats'], ['ambient'], function (bot, message) {
+	bot.reply(message, '```HEY! YOU JUST SAID BETSY\'S MAGIC WORD\n\n @@@@@@@@   @@@@@@    @@@@@@   @@@@@@@   @@@@@@\n@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  @@@@@@@\n!@@        @@!  @@@  @@!  @@@    @@!    !@@\n!@!        !@!  @!@  !@!  @!@    !@!    !@!\n!@! @!@!@  @!@  !@!  @!@!@!@!    @!!    !!@@!!\n!!! !!@!!  !@!  !!!  !!!@!!!!    !!!     !!@!!!\n:!!   !!:  !!:  !!!  !!:  !!!    !!:         !:!\n:!:   !::  :!:  !:!  :!:  !:!    :!:        !:!\n ::: ::::  ::::: ::  ::   :::     ::    :::: ::\n :: :: :    : :  :    :   : :     :     :: : :\n```')
+})
+
 controller.hears('.*', ['direct_message'], function (bot, message) {
 	bot.reply(message, "I'm more of a group bot.")
 })
